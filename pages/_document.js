f229282c68753d62c505f2f5065c1eae3ffdab31
varcsoft <varcsoft@gmail.com>
2024-09-04 13:09:11 +0530

    Your commit message

--- pages/_document.js
+++ pages/_document.js
@@ -1,9 +1,13 @@
-import { Html, Head, Main, NextScript } from "next/document";
+import { Html, Head, Main, NextScript } from 'next/document';
 
 export default function Document() {
   return (
     <Html lang="en">
-      <Head />
+      <Head>
+        <link rel="icon" href="/favicon.ico" />
+        <link href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;600;700&display=swap" rel="stylesheet" />
+        <meta name="description" content="Portfolio of Shivakumar Kokkula" />
+      </Head>
       <body>
         <Main />
         <NextScript />
